e9a6fdbd3e66a047aae431e5fba02dca803c8a9d
axlecrusher <axlecrusher@141cdb93-e927-4a07-a99f-75eb1556cfc9>
2008-12-25 01:15:26 +0000

    Aligned buffer template

---
+++ Mercury2/src/AlignedBuffer.h
@@ -0,0 +1,74 @@
+
+template <typename T>
+class AlignedBuffer
+{
+	public:
+		AlignedBuffer()
+			:m_length(0), m_data(NULL), m_mem(NULL)
+		{};
+		
+		~AlignedBuffer()
+		{
+			Free();
+		}
+		
+		void Allocate(unsigned long count)
+		{
+			SAFE_FREE(m_mem);
+			m_data = (T*)mmemalign(32, sizeof(T)*count, (void*&)m_mem);
+			m_length = count;
+		}
+		
+		void Free()
+		{
+			SAFE_FREE(m_mem);
+			m_data = NULL;
+			m_length = 0;
+		}
+		
+		inline unsigned long Length() { return m_length; }
+		inline unsigned long LengthInBytes() { return m_length*sizeof(T); }
+		
+		inline T* Buffer() { return m_data; }
+		
+		T& operator[](unsigned long x)
+		{
+			return m_data[x];
+		}
+		
+	private:
+		unsigned long m_length;
+		T* m_data, *m_mem;
+};
+
+/****************************************************************************
+ *   Copyright (C) 2008 by Joshua Allen                                     *
+ *                                                                          *
+ *                                                                          *
+ *   All rights reserved.                                                   *
+ *                                                                          *
+ *   Redistribution and use in source and binary forms, with or without     *
+ *   modification, are permitted provided that the following conditions     *
+ *   are met:                                                               *
+ *     * Redistributions of source code must retain the above copyright     *
+ *      notice, this list of conditions and the following disclaimer.       *
+ *     * Redistributions in binary form must reproduce the above            *
+ *      copyright notice, this list of conditions and the following         *
+ *      disclaimer in the documentation and/or other materials provided     *
+ *      with the distribution.                                              *
+ *     * Neither the name of the Mercury Engine nor the names of its        *
+ *      contributors may be used to endorse or promote products derived     *
+ *      from this software without specific prior written permission.       *
+ *                                                                          *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    *
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      *
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  *
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   *
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  *
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       *
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  *
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  *
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    *
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  *
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   *
+ ***************************************************************************/
